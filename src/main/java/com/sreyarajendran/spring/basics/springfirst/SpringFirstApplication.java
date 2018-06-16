4a60622b4f4208ca51876253b60171727a98564c
Sreya Rajendran <Sreya Rajendran@DESKTOP-O31F3P0>
2018-06-16 00:36:25 -0700

    First commit
    Created simple spring example that doesn't really do anything

---
+++ src/main/java/com/sreyarajendran/spring/basics/springfirst/SpringFirstApplication.java
@@ -0,0 +1,12 @@
+package com.sreyarajendran.spring.basics.springfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFirstApplication.class, args);
+	}
+}
